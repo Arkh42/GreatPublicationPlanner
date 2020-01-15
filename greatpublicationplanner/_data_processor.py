cbca2909e78dcd720645548ff8ba7d3fc9e12aa1
Arkh42 <aquenon@hotmail.be>
2020-01-15 17:09:25 +0100

    Creation of a data processing library for checking integrity and formatting data.

---
+++ greatpublicationplanner/_data_processor.py
@@ -0,0 +1,48 @@
+
+# Copyright (c) 2020 Alexandre Quenon - UMONS
+
+"""
+Process the data (integrity check, sort, filter) so that the drawer has always the same data format.
+"""
+
+
+
+import pandas as pd
+
+
+# Data modification
+def make_lower_case_header(data):
+    data.columns = map(str.lower, data.columns)
+
+# Data integrity check: low-level functions
+def has_name(data):
+    return 'name' in data.columns
+
+def has_start_date(data):
+    return 'start' in data.columns
+
+def has_end_date(data):
+    return 'end' in data.columns
+
+def has_submission_date(data):
+    return 'submission' in data.columns
+
+# Data integrity check: high-level functions
+def check_data_integrity(data):
+    missing_fields = []
+
+    if not has_name(data):
+        missing_fields.append('name')
+    if not has_start_date(data):
+        missing_fields.append('start')
+    if not has_end_date(data):
+        missing_fields.append('end')
+    if not has_submission_date(data):
+        missing_fields.append('submission')
+    
+    if missing_fields:
+        raise KeyError('Mandatory fields are missing in data: {}'.format(missing_fields))
+
+# Sort functions
+def sort_on_start_date(data, ascending=True):
+    return data.sort_values(by='start', ascending=ascending)
---
+++ greatpublicationplanner/tests/test_data_processor.py
@@ -0,0 +1,85 @@
+
+# Copyright (c) 2020 Alexandre Quenon - UMONS
+
+
+
+import unittest
+
+
+import greatpublicationplanner._data_processor as processor
+import greatpublicationplanner._file_manager as file_manager
+
+
+class DataProcessorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.data_raw = file_manager.open_file_shell('./greatpublicationplanner/tests/examples/short_list_conf.csv')
+        self.data_ok = file_manager.open_file_shell('./greatpublicationplanner/tests/examples/short_list_conf.csv')
+        processor.make_lower_case_header(self.data_ok)
+
+
+    # Test suite: data modification
+    def test_make_lower_case_header(self):
+        processor.make_lower_case_header(self.data_raw)
+        for header in self.data_raw:
+            self.assertTrue(header.islower())
+    
+
+    # Test suite: data integrity
+    def test_has_name__false(self):
+        data_corrupted = self.data_ok.rename(columns={'name':'toto'})
+        self.assertFalse(processor.has_name(data_corrupted))
+
+    def test_has_name__true(self):
+        self.assertTrue(processor.has_name(self.data_ok))
+
+
+    def test_has_start_date__false(self):
+        data_corrupted = self.data_ok.rename(columns={'start':'toto'})
+        self.assertFalse(processor.has_start_date(data_corrupted))
+
+    def test_has_start_date__true(self):
+        self.assertTrue(processor.has_start_date(self.data_ok))
+    
+
+    def test_has_end_date__false(self):
+        data_corrupted = self.data_ok.rename(columns={'end':'toto'})
+        self.assertFalse(processor.has_end_date(data_corrupted))
+
+    def test_has_end_date__true(self):
+        self.assertTrue(processor.has_end_date(self.data_ok))
+    
+
+    def test_has_submission_date__false(self):
+        data_corrupted = self.data_ok.rename(columns={'submission':'toto'})
+        self.assertFalse(processor.has_submission_date(data_corrupted))
+
+    def test_has_submission_date__true(self):
+        self.assertTrue(processor.has_submission_date(self.data_ok))
+    
+
+    def test_check_data_integrity__missing_name(self):
+        data_corrupted = self.data_ok.rename(columns={'name':'toto'})
+        with self.assertRaises(KeyError):
+            processor.check_data_integrity(data_corrupted)
+    
+    def test_check_data_integrity__missing_start_date(self):
+        data_corrupted = self.data_ok.rename(columns={'start':'toto'})
+        with self.assertRaises(KeyError):
+            processor.check_data_integrity(data_corrupted)
+    
+    def test_check_data_integrity__missing_end_date(self):
+        data_corrupted = self.data_ok.rename(columns={'end':'toto'})
+        with self.assertRaises(KeyError):
+            processor.check_data_integrity(data_corrupted)
+    
+    def test_check_data_integrity__missing_submission_date(self):
+        data_corrupted = self.data_ok.rename(columns={'submission':'toto'})
+        with self.assertRaises(KeyError):
+            processor.check_data_integrity(data_corrupted)
+    
+    def test_check_data_integrity__ok(self):
+        try:
+            processor.check_data_integrity(self.data_ok)
+        except KeyError:
+            self.fail('An exception was thrown but not expected')
